57d35fcdcfbcab6f952d75c52feae2c9604f6a5d
czechoa <andrzej.czechu.ja@gmail.com>
2022-11-29 11:49:23 +0100

    Initial commit

---
+++ coco_text/open_json.py
@@ -0,0 +1,8 @@
+import json
+annotation_filename = "COCO_Text.json"
+
+with open(annotation_filename) as json_file:
+    coco_text = json.load(json_file)
+
+print(coco_text["cats"])
+print(coco_text.keys())
