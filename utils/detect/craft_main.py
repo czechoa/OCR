1d9df1b74af0205d6146fe147adb9e2d8257681a
czechoa <andrzej.czechu.ja@gmail.com>
2022-11-30 16:17:59 +0100

    code clean up in craft

--- utils/detect/craft_main.py
+++ utils/detect/craft_main.py
@@ -1,5 +1,32 @@
-from utils.detect.craft_pytorch.main import main
+# from datetime import time
+import time
+import numpy as np
+import torch
+
+from utils.detect.craft_pytorch import imgproc
+from utils.detect.craft_pytorch.craft import CRAFT
+from utils.detect.craft_pytorch.test_net import test_net, copyStateDict
 
 
-def craft_main(images, trained_model_path):
-    return main(images, trained_model=trained_model_path)
+def craft_main(image_path, trained_model='saved_models/craft_mlt_25k.pth', text_threshold=0.7, low_text=0.4,
+               link_threshold=0.4, cuda=False, canvas_size=1280, mag_ratio=1.5, poly=False, show_time=False):
+    # load net
+    net = CRAFT()  # initialize
+
+    print('Loading weights from checkpoint (' + trained_model + ')')
+
+    net.load_state_dict(copyStateDict(torch.load(trained_model, map_location='cpu')))
+
+    net.eval()
+
+    t = time.time()
+
+    image = imgproc.loadImage(image_path)
+
+    bboxes, polys, score_text = test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, canvas_size,
+                                         mag_ratio, show_time)
+
+    # score_text_list.append(score_text)
+    print("elapsed time : {}s".format(time.time() - t))
+
+    return np.hstack([bboxes[:, 0], np.ceil(bboxes[:, -2] - bboxes[:, 0])]).astype(int)
--- utils/detect/craft_pytorch/test_net.py
+++ utils/detect/craft_pytorch/test_net.py
@@ -16,8 +16,6 @@ from utils.detect.craft_pytorch import craft_utils
 from utils.detect.craft_pytorch import imgproc
 from utils.detect.craft_pytorch.craft import CRAFT
 
-
-# from utils.detect.craft_pytorch import file_utils
 def copyStateDict(state_dict):
     if list(state_dict.keys())[0].startswith("module"):
         start_idx = 1
@@ -28,10 +26,6 @@ def copyStateDict(state_dict):
         name = ".".join(k.split(".")[start_idx:])
         new_state_dict[name] = v
     return new_state_dict
-
-
-def str2bool(v):
-    return v.lower() in ("yes", "y", "true", "t", "1")
 
 
 def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, canvas_size, mag_ratio, show_time,
@@ -89,65 +83,65 @@ def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, c
     return boxes, polys, ret_score_text
 
 
-def main(image_path, trained_model='weights/craft_mlt_25k.pth', text_threshold=0.7, low_text=0.4, link_threshold=0.4,
-         cuda=False, canvas_size=1280, mag_ratio=1.5, poly=False, show_time=False, test_folder='/data/', refine=False,
-         refiner_model='weights/craft_refiner_CTW1500.pth'):
-    # load net
-    net = CRAFT()  # initialize
-
-    print('Loading weights from checkpoint (' + trained_model + ')')
-    if cuda:
-        net.load_state_dict(copyStateDict(torch.load(trained_model)))
-    else:
-        net.load_state_dict(copyStateDict(torch.load(trained_model, map_location='cpu')))
-
-    if cuda:
-        net = net.cuda()
-        net = torch.nn.DataParallel(net)
-        cudnn.benchmark = False
-
-    net.eval()
-
-    # LinkRefiner
-    refine_net = None
-    if refine:
-        from refinenet import RefineNet
-        refine_net = RefineNet()
-        print('Loading weights of refiner from checkpoint (' + refiner_model + ')')
-        if cuda:
-            refine_net.load_state_dict(copyStateDict(torch.load(refiner_model)))
-            refine_net = refine_net.cuda()
-            refine_net = torch.nn.DataParallel(refine_net)
-        else:
-            refine_net.load_state_dict(copyStateDict(torch.load(refiner_model, map_location='cpu')))
-
-        refine_net.eval()
-        poly = True
-
-    t = time.time()
-
-    # load data
-    score_text_list = []
-
-    # for k, image_path in enumerate(image_list):
-    image = imgproc.loadImage(image_path)
-
-
-
-    bboxes, polys, score_text = test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly,
-                                         canvas_size, mag_ratio, show_time, refine_net)
-
-        # score_text_list.append(score_text)
-    print("elapsed time : {}s".format(time.time() - t))
-
-
-    return np.hstack([bboxes[:, 0], np.ceil(bboxes[:, -2] - bboxes[:, 0])]).astype(int)
-        # save score text
-        # filename, file_ext = os.path.splitext(os.path.basename(image_path))
-        # mask_file = result_folder + "/res_" + filename + '_mask.jpg'
-        # cv2.imwrite(mask_file, score_text)
-        #
-        # file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
-
-    #
-    # return score_text_list, polys, bboxes
+# def main(image_path, trained_model='weights/craft_mlt_25k.pth', text_threshold=0.7, low_text=0.4, link_threshold=0.4,
+#          cuda=False, canvas_size=1280, mag_ratio=1.5, poly=False, show_time=False, test_folder='/data/', refine=False,
+#          refiner_model='weights/craft_refiner_CTW1500.pth'):
+#     # load net
+#     net = CRAFT()  # initialize
+#
+#     print('Loading weights from checkpoint (' + trained_model + ')')
+#     if cuda:
+#         net.load_state_dict(copyStateDict(torch.load(trained_model)))
+#     else:
+#         net.load_state_dict(copyStateDict(torch.load(trained_model, map_location='cpu')))
+#
+#     if cuda:
+#         net = net.cuda()
+#         net = torch.nn.DataParallel(net)
+#         cudnn.benchmark = False
+#
+#     net.eval()
+#
+#     # LinkRefiner
+#     refine_net = None
+#     if refine:
+#         from refinenet import RefineNet
+#         refine_net = RefineNet()
+#         print('Loading weights of refiner from checkpoint (' + refiner_model + ')')
+#         if cuda:
+#             refine_net.load_state_dict(copyStateDict(torch.load(refiner_model)))
+#             refine_net = refine_net.cuda()
+#             refine_net = torch.nn.DataParallel(refine_net)
+#         else:
+#             refine_net.load_state_dict(copyStateDict(torch.load(refiner_model, map_location='cpu')))
+#
+#         refine_net.eval()
+#         poly = True
+#
+#     t = time.time()
+#
+#     # load data
+#     score_text_list = []
+#
+#     # for k, image_path in enumerate(image_list):
+#     image = imgproc.loadImage(image_path)
+#
+#
+#
+#     bboxes, polys, score_text = test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly,
+#                                          canvas_size, mag_ratio, show_time, refine_net)
+#
+#         # score_text_list.append(score_text)
+#     print("elapsed time : {}s".format(time.time() - t))
+#
+#
+#     return np.hstack([bboxes[:, 0], np.ceil(bboxes[:, -2] - bboxes[:, 0])]).astype(int)
+#         # save score text
+#         # filename, file_ext = os.path.splitext(os.path.basename(image_path))
+#         # mask_file = result_folder + "/res_" + filename + '_mask.jpg'
+#         # cv2.imwrite(mask_file, score_text)
+#         #
+#         # file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
+#
+#     #
+#     # return score_text_list, polys, bboxes
