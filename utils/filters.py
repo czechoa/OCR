27f3d9638d9f09b1131bd3aded1b32665c5306f7
czechoa <andrzej.czechu.ja@gmail.com>
2022-12-06 11:45:24 +0100

    complete example for image with plane

--- utils/filters.py
+++ utils/filters.py
@@ -1,14 +1,13 @@
 import cv2
+import numpy as np
 
-
-def median_filtering(img):
-    return cv2.medianBlur(img,3)
+def median_filtering(img, neighbor=3):
+    print(neighbor)
+    return cv2.medianBlur(img,neighbor)
 
 def bilateral_filtering(img):
     return cv2.bilateralFilter(img, 9, 75, 75)
 
-def gaussian_blurring(img):
-    return cv2.bilateralFilter(img,9,75,75)
 
 
 def to_threshold_binary(img):
@@ -17,3 +16,11 @@ def to_threshold_binary(img):
     #
     (T, thresh) = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
     return thresh
+
+
+def image_processing_clean_up(image):
+    norm_img = np.zeros((image.shape[0], image.shape[1]))
+    image = cv2.normalize(image, norm_img, 0, 255, cv2.NORM_MINMAX)
+    image = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)[1]
+    image = cv2.GaussianBlur(image, (1, 1), 0)
+    return image
